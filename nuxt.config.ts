e07e1af723db8ab65a27ce484c3a1484b7a50f77
Daniel Lemper <ich@dlemper.de>
2023-12-04 17:20:04 +0100

    add nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,8 @@
 export default defineNuxtConfig({
   ssr: false, // Must be false to be used served by pocketbase
   srcDir: 'app',
-  css: [
-    'primevue/resources/themes/saga-blue/theme.css',
-    'primevue/resources/primevue.css',
-    'primeicons/primeicons.css',
-    'primeflex/primeflex.css'
-  ],
-  build: {
-    transpile: ['primevue']
-  },
+  modules: ['@nuxt/ui'],
   imports: {
-    autoImport: true,
     addons: {
       vueTemplate: true
     }
